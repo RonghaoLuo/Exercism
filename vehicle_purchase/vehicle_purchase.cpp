d7a6ae661e8992c97e8996e493bdf71366a90ff3
RonghaoLuo <ronghaoluo@gmail.com>
2024-12-24 20:19:25 -0800

    Fixed issues and passed all tests on Exercism.

--- vehicle_purchase/vehicle_purchase.cpp
+++ vehicle_purchase/vehicle_purchase.cpp
@@ -16,9 +16,9 @@ namespace vehicle_purchase {
     std::string choose_vehicle(std::string option1, std::string option2) {
         // TODO: Return the final decision in a sentence.
         if (option1 < option2) { // <= not necessary, since if equal, either is fine
-            return option1;
+            return option1 + " is clearly the better choice.";
         } else {
-            return option2;
+            return option2 + " is clearly the better choice.";
         }
     }
 
@@ -29,7 +29,7 @@ namespace vehicle_purchase {
             return original_price * 0.8;
         } else if (age >= 3 && age < 10) {
             return original_price * 0.7;
-        } else if (age > 10) {
+        } else if (age >= 10) {
             return original_price * 0.5;
         } else {
             return -1; // for invalid input
