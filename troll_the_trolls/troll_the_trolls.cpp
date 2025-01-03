b6b188a5de717fa1ec3872926a2524978dc2b733
RonghaoLuo <ronghaoluo@gmail.com>
2025-01-02 17:13:13 -0800

    Done troll_the_trolls

--- troll_the_trolls/troll_the_trolls.cpp
+++ troll_the_trolls/troll_the_trolls.cpp
@@ -55,13 +55,100 @@ bool display_post(AccountStatus poster, AccountStatus viewer) {
 // `Action` as a first argument and an `AccountStatus` to check against. It
 // should return a `bool`.
 
+bool permission_check(Action action, AccountStatus account) {
+    if (account == AccountStatus::mod) {
+        return true;
+    } else if (account == AccountStatus::user || account == AccountStatus::troll) {
+
+    }
+
+    switch (account) {
+        case AccountStatus::mod:
+            return true;
+        case AccountStatus::user:
+        case AccountStatus::troll:
+            switch (action) {
+                case Action::read:
+                case Action::write:
+                    return true;        
+                default:
+                    return false;
+            }
+        case AccountStatus::guest:
+            switch (action) {
+                case Action::read:
+                    return true;
+                default:
+                    return false;
+            }
+        default:
+            return false; // or I should throw exception for any unexpected?
+    }
+}
+
 // TODO: Task 4 - Implement the `valid_player_combination` function that
 // checks if two players can join the same game. The function has two parameters
 // of type `AccountStatus` and returns a `bool`.
+
+void validate_AccountStatus(AccountStatus account) {
+    switch (account) {
+        case AccountStatus::guest:
+        case AccountStatus::mod:
+        case AccountStatus::troll:
+        case AccountStatus::user:
+            break;
+        default:
+            break; // throw error
+    }
+}
+
+// combinations of the following return false, otherwise true: (guest, x), (troll, !troll)
+bool valid_player_combination(AccountStatus player_1, AccountStatus player_2) {
+    // check inputs are valid
+    validate_AccountStatus(player_1);
+    validate_AccountStatus(player_2);
+
+    if (player_1 == AccountStatus::guest || player_2 == AccountStatus::guest) {
+        return false;
+    } else if ((player_1 == AccountStatus::troll && player_2 != AccountStatus::troll) ||
+               (player_2 == AccountStatus::troll && player_1 != AccountStatus::troll)) {
+        return false;
+    } else {
+        return true;
+    }
+}
 
 // TODO: Task 5 - Implement the `has_priority` function that takes two
 // `AccountStatus` arguments and returns `true`, if and only if the first
 // account has a strictly higher priority than the second.
 
+bool has_priority(AccountStatus account_1, AccountStatus account_2) {
+    // check inputs are valid
+    validate_AccountStatus(account_1);
+    validate_AccountStatus(account_2);
+
+    switch (account_1) {
+        case AccountStatus::mod:
+            if (account_2 != AccountStatus::mod) {
+                return true;
+            } else {
+                return false;
+            }
+        case AccountStatus::user:
+            if (account_2 == AccountStatus::guest || account_2 == AccountStatus::troll) {
+                return true;
+            } else {
+                return false;
+            }
+        case AccountStatus::guest:
+            if (account_2 == AccountStatus::troll) {
+                return true;
+            } else {
+                return false;
+            }
+        default: // troll accounts have no strictly higher priority
+            return false;
+    }
+}
 
 }  // namespace hellmath
